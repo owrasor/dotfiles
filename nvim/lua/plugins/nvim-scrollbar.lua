70c9116c61b8f650428d298928e520f947124c42
Fabio Leal Kretzer <owrasor@gmail.com>
2025-03-29 11:54:35 -0300

    removendo scrollbar

--- nvim/lua/plugins/nvim-scrollbar.lua
+++ nvim/lua/plugins/nvim-scrollbar.lua
@@ -1,10 +1,10 @@
 return {
-	"petertriho/nvim-scrollbar",
-	config = function()
-		local colors = require("tokyonight.colors").setup()
-
-		require("scrollbar").setup({
-			colors = colors.bg_highlight,
-		})
-	end,
+	-- 	"petertriho/nvim-scrollbar",
+	-- 	config = function()
+	-- 		local colors = require("tokyonight.colors").setup()
+	--
+	-- 		require("scrollbar").setup({
+	-- 			colors = colors.bg_highlight,
+	-- 		})
+	-- 	end,
 }
