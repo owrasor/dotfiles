9d440cffceea2949e8f81f8027690d1db99c6570
Fabio Leal Kretzer <owrasor@gmail.com>
2025-05-14 14:37:38 -0300

    feat: addicionando fugitive

--- nvim/lua/plugins/artificial-inteligence.lua
+++ nvim/lua/plugins/artificial-inteligence.lua
@@ -1,28 +1,54 @@
 return {
 	{
-		"augmentcode/augment.vim",
-		config = function()
-			vim.g.augment_workspace_folders = {
-				"/Users/owrasor/Code/aprovafacil/aprovafacil-webapp",
-				"/Users/owrasor/Code/aprovafacil/aprovafacil-webserver",
-				"/Users/owrasor/Code/aprovafacil/aprovafacil-serverjs",
-				"/Users/owrasor/Code/aprovafacil/aprovafacil-infra-dev",
-				"/Users/owrasor/Code/aprovafacil/aprovafacil-infra-ec2",
-				"/Users/owrasor/Code/owrasor/personal-finances",
-			}
-
-			local keymap = vim.keymap.set
-
-			-- Open the chat window
-			keymap({ "n", "v" }, "<leader>ac", "<cmd>Augment chat<CR>", { desc = "Open Augment Chat" })
-
-			-- Toggle chat window visibility
-			keymap("n", "<leader>at", "<cmd>Augment chat-toggle<CR>", { desc = "Toggle Augment Chat" })
-
-			-- (Optional) Start a new chat session
-			keymap("n", "<leader>as", "<cmd>Augment chat-new<CR>", { desc = "Start Augment Chat" })
-		end,
+		"github/copilot.vim",
 	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		keys = {
+			{ "<leader>ac", ":CopilotChat<CR>", mode = "n", desc = "Chat With Copilot" },
+			{ "<leader>ae", ":CopilotChatExplain<CR>", mode = "v", desc = "Explain Code" },
+			{ "<leader>ar", ":CopilotChatReview<CR>", mode = "v", desc = "Review Code" },
+			{ "<leader>af", ":CopilotChatFix<CR>", mode = "v", desc = "Fix Code Issues" },
+			{ "<leader>ao", ":CopilotChatOptimize<CR>", mode = "v", desc = "Optimize Code" },
+			{ "<leader>ad", ":CopilotChatDocs<CR>", mode = "v", desc = "Generate Docs" },
+			{ "<leader>at", ":CopilotChatTests<CR>", mode = "v", desc = "Generate Tests" },
+			{ "<leader>am", ":CopilotChatCommit<CR>", mode = "n", desc = "Generate Commit Message" },
+			{ "<leader>as", ":CopilotChatCommit<CR>", mode = "v", desc = "Generate Commit for Selection" },
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	},
+	-- {
+	-- 	"augmentcode/augment.vim",
+	-- 	config = function()
+	-- 		vim.g.augment_workspace_folders = {
+	-- 			"/Users/owrasor/Code/aprovafacil/aprovafacil-webapp",
+	-- 			"/Users/owrasor/Code/aprovafacil/aprovafacil-webserver",
+	-- 			"/Users/owrasor/Code/aprovafacil/aprovafacil-serverjs",
+	-- 			"/Users/owrasor/Code/aprovafacil/aprovafacil-infra-dev",
+	-- 			"/Users/owrasor/Code/aprovafacil/aprovafacil-infra-ec2",
+	-- 			"/Users/owrasor/Code/owrasor/personal-finances",
+	-- 		}
+	--
+	-- 		local keymap = vim.keymap.set
+	--
+	-- 		-- Open the chat window
+	-- 		keymap({ "n", "v" }, "<leader>ac", "<cmd>Augment chat<CR>", { desc = "Open Augment Chat" })
+	--
+	-- 		-- Toggle chat window visibility
+	-- 		keymap("n", "<leader>at", "<cmd>Augment chat-toggle<CR>", { desc = "Toggle Augment Chat" })
+	--
+	-- 		-- (Optional) Start a new chat session
+	-- 		keymap("n", "<leader>as", "<cmd>Augment chat-new<CR>", { desc = "Start Augment Chat" })
+	-- 	end,
+	-- },
 	-- {
 	-- 	"yetone/avante.nvim",
 	-- 	event = "VeryLazy",
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -27,7 +27,7 @@ return {
 			})
 
 			mason_lspconfig.setup({
-				automatic_instalation = true,
+				automatic_installation = true,
 				-- list of servers for mason to install
 				ensure_installed = {
 					"ts_ls",
@@ -215,10 +215,14 @@ return {
 					keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
 					opts.desc = "Go to previous diagnostic"
-					keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+					keymap.set("n", "[d", function()
+						vim.diagnostic.jump({ count = -1, float = true })
+					end, opts) -- jump to previous diagnostic in buffer
 
 					opts.desc = "Go to next diagnostic"
-					keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+					keymap.set("n", "]d", function()
+						vim.diagnostic.jump({ count = 1, float = true })
+					end, opts) -- jump to next diagnostic in buffer
 
 					opts.desc = "Show documentation for what is under cursor"
 					keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
