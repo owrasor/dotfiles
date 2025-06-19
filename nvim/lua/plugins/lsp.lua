d47fde5ac3859add6194a155b9f5ff4c772ae9e1
Fabio Leal Kretzer <owrasor@gmail.com>
2025-06-19 17:00:36 -0300

    feat: adicionando plugin para melhor funcionamento do lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,7 +7,24 @@ return {
 			{
 				"mason-org/mason-lspconfig.nvim",
 				tag = "v2.0.0",
-				dependencies = { "neovim/nvim-lspconfig" },
+				dependencies = {
+					{
+						"neovim/nvim-lspconfig",
+						dependencies = {
+							{
+								"folke/lazydev.nvim",
+								ft = "lua", -- only load on lua files
+								opts = {
+									library = {
+										-- See the configuration section for more details
+										-- Load luvit types when the `vim.uv` word is found
+										{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+									},
+								},
+							},
+						},
+					},
+				},
 			},
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 			{ "antosha417/nvim-lsp-file-operations", config = true },
