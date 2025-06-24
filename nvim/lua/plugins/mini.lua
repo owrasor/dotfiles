79f218fc64f79cc12a107f9943cb99e0c5a243ab
Fabio Leal Kretzer <owrasor@gmail.com>
2025-06-24 15:53:36 -0300

    fix: retomando o mini.move para mover o texto com mais rapidez

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -4,6 +4,18 @@ return {
 		version = "*",
 		event = "VeryLazy",
 		opts = {},
-		config = function() end,
+		config = function()
+			require("mini.move").setup({
+				mappings = {
+					-- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+					down = "<M-Down>",
+					up = "<M-Up>",
+
+					-- Move current line in Normal mode
+					line_down = "<M-Down>",
+					line_up = "<M-Up>",
+				},
+			})
+		end,
 	},
 }
