ea373b6f34a8e6ca397bb89738dd9f945d0e6c11
Fabio Leal Kretzer <owrasor@gmail.com>
2026-02-26 15:24:37 -0300

    fix: ajustados pontos para instalação.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,30 +9,30 @@ config.font = wezterm.font("FiraCode Nerd Font")
 config.color_scheme = "tokyonight_night"
 
 config.colors = {
-	cursor_bg = "#7aa2f7",
-	cursor_border = "7aa2f7",
+    cursor_bg = "#7aa2f7",
+    cursor_border = "7aa2f7",
 }
 
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
 config.window_padding = {
-	left = 10,
-	right = 2,
-	top = 2,
-	bottom = 2,
+    left = 10,
+    right = 2,
+    top = 2,
+    bottom = 2,
 }
 
 wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():maximize()
 end)
 
 config.keys = {
-	{
-		key = "n",
-		mods = "SHIFT|CTRL",
-		action = wezterm.action.ToggleFullScreen,
-	},
+    {
+        key = "n",
+        mods = "SHIFT|CTRL",
+        action = wezterm.action.ToggleFullScreen,
+    },
 }
 
 -- config.default_domain = "WSL:Ubuntu"
